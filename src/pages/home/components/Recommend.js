3c6a5ab6bd7d4c5de660036918d2e90e7e0ecbb6
Joern <924901972@qq.com>
2020-02-06 21:13:14 +0800

    feat: 添加reducer中

--- src/pages/home/components/Recommend.js
+++ src/pages/home/components/Recommend.js
@@ -1,14 +1,25 @@
 import React, {Component} from 'react';
 import {RecommendItem, RecommendWrapper} from "../style";
+import {connect} from "react-redux";
 
-class Recommend extends Component{
-    render(){
-        return (
-            <RecommendWrapper>
-                <RecommendItem imgUrl='http://cdn2.jianshu.io/assets/web/banner-s-club-aa8bdf19f8cf729a759da42e4a96f366.png'/>
-                <RecommendItem imgUrl='http://cdn2.jianshu.io/assets/web/banner-s-7-1a0222c91694a1f38e610be4bf9669be.png'/>
-            </RecommendWrapper>
-        );
-    }
+class Recommend extends Component {
+  render() {
+    return (
+      <RecommendWrapper>
+        {
+          this.props.list.map((item) => {
+            return (
+              <RecommendItem imgUrl={item.get('imgUrl')} key={item.get('id')}/>
+            );
+          })
+        }
+      </RecommendWrapper>
+    );
+  }
 }
-export default Recommend;
+
+const mapStateToProps = (state) => ({
+  list: state.getIn(['home', 'recommendList'])
+});
+
+export default connect(mapStateToProps, null)(Recommend);
--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -1,33 +1,40 @@
 import {fromJS} from 'immutable';
 
 const defaultState = fromJS({
-   topicList: [{
-       id:1,
-       title:'社会热点',
-       imgUrl:'https://upload-images.jianshu.io/upload_images/1741702-7d340c9913d5ada8.jpg?imageMogr2/auto-orient/strip|imageView2/2/w/424/format/webp'
-   },{
-       id:2,
-       title:'手绘',
-       imgUrl:'https://upload-images.jianshu.io/upload_images/1741702-7d340c9913d5ada8.jpg?imageMogr2/auto-orient/strip|imageView2/2/w/424/format/webp'
-   }],
-    articleList:[{
-       id:1,
-        title:'Python大佬实战APP抓包，抖音的小姐姐等着我！',
-        desc:'APP抓包 前面我们了解了一些关于 Python 爬虫的知识，不过都是基于 PC 端浏览器网页中的内容进行爬取。现在手机 App 用的越来越多，...',
-        imgUrl:'https://upload-images.jianshu.io/upload_images/13448242-20871e9f865fb242.jpg?imageMogr2/auto-orient/strip|imageView2/2/w/594/format/webp'
-    },{
-        id:2,
-        title:'Python大佬实战APP抓包，抖音的小姐姐等着我！',
-        desc:'APP抓包 前面我们了解了一些关于 Python 爬虫的知识，不过都是基于 PC 端浏览器网页中的内容进行爬取。现在手机 App 用的越来越多，...',
-        imgUrl:'https://upload-images.jianshu.io/upload_images/13448242-20871e9f865fb242.jpg?imageMogr2/auto-orient/strip|imageView2/2/w/594/format/webp'
-    },{
-        id:3,
-        title:'Python大佬实战APP抓包，抖音的小姐姐等着我！',
-        desc:'APP抓包 前面我们了解了一些关于 Python 爬虫的知识，不过都是基于 PC 端浏览器网页中的内容进行爬取。现在手机 App 用的越来越多，...',
-        imgUrl:'https://upload-images.jianshu.io/upload_images/13448242-20871e9f865fb242.jpg?imageMogr2/auto-orient/strip|imageView2/2/w/594/format/webp'
-    }]
+  topicList: [{
+    id: 1,
+    title: '社会热点',
+    imgUrl: 'https://upload-images.jianshu.io/upload_images/1741702-7d340c9913d5ada8.jpg?imageMogr2/auto-orient/strip|imageView2/2/w/424/format/webp'
+  }, {
+    id: 2,
+    title: '手绘',
+    imgUrl: 'https://upload-images.jianshu.io/upload_images/1741702-7d340c9913d5ada8.jpg?imageMogr2/auto-orient/strip|imageView2/2/w/424/format/webp'
+  }],
+  articleList: [{
+    id: 1,
+    title: 'Python大佬实战APP抓包，抖音的小姐姐等着我！',
+    desc: 'APP抓包 前面我们了解了一些关于 Python 爬虫的知识，不过都是基于 PC 端浏览器网页中的内容进行爬取。现在手机 App 用的越来越多，...',
+    imgUrl: 'https://upload-images.jianshu.io/upload_images/13448242-20871e9f865fb242.jpg?imageMogr2/auto-orient/strip|imageView2/2/w/594/format/webp'
+  }, {
+    id: 2,
+    title: 'Python大佬实战APP抓包，抖音的小姐姐等着我！',
+    desc: 'APP抓包 前面我们了解了一些关于 Python 爬虫的知识，不过都是基于 PC 端浏览器网页中的内容进行爬取。现在手机 App 用的越来越多，...',
+    imgUrl: 'https://upload-images.jianshu.io/upload_images/13448242-20871e9f865fb242.jpg?imageMogr2/auto-orient/strip|imageView2/2/w/594/format/webp'
+  }, {
+    id: 3,
+    title: 'Python大佬实战APP抓包，抖音的小姐姐等着我！',
+    desc: 'APP抓包 前面我们了解了一些关于 Python 爬虫的知识，不过都是基于 PC 端浏览器网页中的内容进行爬取。现在手机 App 用的越来越多，...',
+    imgUrl: 'https://upload-images.jianshu.io/upload_images/13448242-20871e9f865fb242.jpg?imageMogr2/auto-orient/strip|imageView2/2/w/594/format/webp'
+  }],
+  recommendList: [{
+    id: 1,
+    imgUrl: 'http://cdn2.jianshu.io/assets/web/banner-s-club-aa8bdf19f8cf729a759da42e4a96f366.png'
+  }, {
+    id: 2,
+    imgUrl: 'http://cdn2.jianshu.io/assets/web/banner-s-7-1a0222c91694a1f38e610be4bf9669be.png'
+  }]
 });
 
 export default (state = defaultState, action) => {
-    return state;
+  return state;
 }
