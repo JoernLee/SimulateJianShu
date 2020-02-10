fb2a7801c0f37bce44fd122af1da31623229e79c
Joern <924901972@qq.com>
2020-02-10 20:07:30 +0800

    feat:添加详情页布局

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,10 +1,22 @@
 import React, {PureComponent} from 'react';
+import {Content, DetailWrapper, Header} from "./styled";
 
-class Detail extends PureComponent{
-    render(){
-        return (
-            <div>Detail</div>
-        );
-    }
+class Detail extends PureComponent {
+  render() {
+    return (
+      <DetailWrapper>
+        <Header>若是余生绵长，却只留她一人寂寥，那该有多残忍。</Header>
+        <Content>
+          <img
+            src='http://upload-images.jianshu.io/upload_images/8202513-00d6c528c21e09be.png?imageMogr2/auto-orient/strip|imageView2/2/w/658/format/webp'/>
+          <p>柳公子听了她的话，深情款款道：“可否换我来照顾你？”</p>
+          <p>柳公子听了她的话，深情款款道：“可否换我来照顾你？”</p>
+          <p>柳公子听了她的话，深情款款道：“可否换我来照顾你？”</p>
+          <p>柳公子听了她的话，深情款款道：“可否换我来照顾你？”</p>
+        </Content>
+      </DetailWrapper>
+    );
+  }
 }
+
 export default Detail;
