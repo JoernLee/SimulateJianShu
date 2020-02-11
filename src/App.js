8e78b871575e6cc2184cb2b6a6dea085c3c96b6d
Joern <924901972@qq.com>
2020-02-11 20:28:05 +0800

    feat:添加登录界面布局

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import store from "./store";
 import {BrowserRouter,Route} from 'react-router-dom';
 import Home from "./pages/home";
 import Detail from "./pages/detail";
+import Login from "./pages/login";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
             <Route path='/' exact component={Home}></Route>
             {/*添加:id，访问detail下要求传递一个id参数，后面link跳转时候进行拼接*/}
             <Route path='/detail/:id' exact component={Detail}></Route>
+            <Route path='/login' exact component={Login}></Route>
           </div>
         </BrowserRouter>
         <GlobalIconfont/>
