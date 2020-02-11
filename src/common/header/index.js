258695c09af011c9019e6c3cb3cbe22f0cf11395
Joern <924901972@qq.com>
2020-02-11 21:24:38 +0800

    feat:登录逻辑实现

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,143 +1,152 @@
 import React, {Component} from 'react';
 import {
-    HeaderWrapper,
-    Logo,
-    Nav,
-    NavItem,
-    NavSearch,
-    Addition,
-    Button,
-    SearchWrapper,
-    SearchInfo,
-    SearchInfoTitle, SearchInfoSwitch, SearchInfoItem, SearchInfoList
+  HeaderWrapper,
+  Logo,
+  Nav,
+  NavItem,
+  NavSearch,
+  Addition,
+  Button,
+  SearchWrapper,
+  SearchInfo,
+  SearchInfoTitle, SearchInfoSwitch, SearchInfoItem, SearchInfoList
 } from "./styles";
 import {CSSTransition} from 'react-transition-group';
 import {connect} from 'react-redux';
 import {actionCreators} from './store/index';
+import {actionCreators as loginActionCreators} from '../../pages/login/store/index';
 import {Link} from "react-router-dom";
 
 
 class Header extends Component {
 
-    getListArea() {
-        const {focused, mouseIn, list, page, totalPage, handleMouseEnter, handleMouseLeave, handleChangePage} = this.props;
-        const newList = list.toJS();
-        const pageList = [];
-        if (newList.length) {
-            for (let i = (page - 1) * 10; i < page * 10; i++) {
-                pageList.push(
-                    //List是immutable类型，所以需要进行上面的toJS转换为普通数组
-                    <SearchInfoItem key={newList[i]}>{newList[i]}</SearchInfoItem>
-                )
-            }
-        }
-        if (focused || mouseIn) {
-            return (
-                <SearchInfo onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-                    <SearchInfoTitle>
-                        热门搜索
-                        <SearchInfoSwitch onClick={() => handleChangePage(page, totalPage, this.spinIcon)}>
-                            <i ref={(icon) => {
-                                this.spinIcon = icon
-                            }} className='iconfont spin'>&#xe851;</i>换一批
-                        </SearchInfoSwitch>
-                    </SearchInfoTitle>
-                    <SearchInfoList>
-                        {pageList}
-                    </SearchInfoList>
-                </SearchInfo>
-            )
-        } else {
-            return null;
-        }
-    };
-
-    render() {
-        const {focused, handleInputFocus, handleInputBlur, list} = this.props;
-        return (
-            <HeaderWrapper>
-                <Link to={'/'}>
-                    <Logo/>
-                </Link>
-                <Nav>
-                    <NavItem className='left active'>首页</NavItem>
-                    <NavItem className='left'>下载App</NavItem>
-                    <NavItem className='right'>登录</NavItem>
-                    <NavItem className='right'>
-                        <i className='iconfont'>&#xe636;</i>
-                    </NavItem>
-                    <SearchWrapper>
-                        <CSSTransition
-                            in={focused}
-                            timeout={200}
-                            classNames="slide"
-                        >
-                            <NavSearch
-                                className={focused ? 'focused' : ''}
-                                onFocus={() => handleInputFocus(list)}
-                                onBlur={handleInputBlur}
-                            >
-                            </NavSearch>
-                        </CSSTransition>
-                        <i className={focused ? 'focused iconfont zoom' : 'iconfont zoom'}>&#xe617;</i>
-                        {this.getListArea()}
-                    </SearchWrapper>
-                </Nav>
-                <Addition>
-                    <Button className='writing'>
-                        <i className='iconfont'>&#xe615;</i>
-                        写文章
-                    </Button>
-                    <Button className='reg'>注册</Button>
-                </Addition>
-            </HeaderWrapper>
+  getListArea() {
+    const {focused, mouseIn, list, page, totalPage, handleMouseEnter, handleMouseLeave, handleChangePage} = this.props;
+    const newList = list.toJS();
+    const pageList = [];
+    if (newList.length) {
+      for (let i = (page - 1) * 10; i < page * 10; i++) {
+        pageList.push(
+          //List是immutable类型，所以需要进行上面的toJS转换为普通数组
+          <SearchInfoItem key={newList[i]}>{newList[i]}</SearchInfoItem>
         )
+      }
     }
+    if (focused || mouseIn) {
+      return (
+        <SearchInfo onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+          <SearchInfoTitle>
+            热门搜索
+            <SearchInfoSwitch onClick={() => handleChangePage(page, totalPage, this.spinIcon)}>
+              <i ref={(icon) => {
+                this.spinIcon = icon
+              }} className='iconfont spin'>&#xe851;</i>换一批
+            </SearchInfoSwitch>
+          </SearchInfoTitle>
+          <SearchInfoList>
+            {pageList}
+          </SearchInfoList>
+        </SearchInfo>
+      )
+    } else {
+      return null;
+    }
+  };
+
+  render() {
+    const {focused, handleInputFocus, handleInputBlur, list, login,logout} = this.props;
+    return (
+      <HeaderWrapper>
+        <Link to={'/'}>
+          <Logo/>
+        </Link>
+        <Nav>
+          <NavItem className='left active'>首页</NavItem>
+          <NavItem className='left'>下载App</NavItem>
+          {
+            login ?
+              <NavItem onClick={logout} className='right'>退出</NavItem> :
+              <Link to='/login'><NavItem className='right'>登录</NavItem></Link>
+          }
+          <NavItem className='right'>
+            <i className='iconfont'>&#xe636;</i>
+          </NavItem>
+          <SearchWrapper>
+            <CSSTransition
+              in={focused}
+              timeout={200}
+              classNames="slide"
+            >
+              <NavSearch
+                className={focused ? 'focused' : ''}
+                onFocus={() => handleInputFocus(list)}
+                onBlur={handleInputBlur}
+              >
+              </NavSearch>
+            </CSSTransition>
+            <i className={focused ? 'focused iconfont zoom' : 'iconfont zoom'}>&#xe617;</i>
+            {this.getListArea()}
+          </SearchWrapper>
+        </Nav>
+        <Addition>
+          <Button className='writing'>
+            <i className='iconfont'>&#xe615;</i>
+            写文章
+          </Button>
+          <Button className='reg'>注册</Button>
+        </Addition>
+      </HeaderWrapper>
+    )
+  }
 
 }
 
 const mapStateToProps = (state) => {
-    return {
-        // focused: state.get('header').get('focused')
-        focused: state.getIn(['header', 'focused']),
-        mouseIn: state.getIn(['header', 'mouseIn']),
-        list: state.getIn(['header', 'list']),
-        page: state.getIn(['header', 'page']),
-        totalPage: state.getIn(['header', 'totalPage'])
-    }
+  return {
+    // focused: state.get('header').get('focused')
+    focused: state.getIn(['header', 'focused']),
+    mouseIn: state.getIn(['header', 'mouseIn']),
+    list: state.getIn(['header', 'list']),
+    page: state.getIn(['header', 'page']),
+    totalPage: state.getIn(['header', 'totalPage']),
+    login: state.getIn(['login', 'login'])
+  }
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        handleInputFocus(list) {
-            (list.size === 0) && dispatch(actionCreators.getList());
-            dispatch(actionCreators.searchFocus());
-        },
-        handleInputBlur() {
-            dispatch(actionCreators.searchBlur());
-        },
-        handleMouseEnter() {
-            dispatch(actionCreators.mouseEnter());
-        },
-        handleMouseLeave() {
-            dispatch(actionCreators.mouseLeave());
-        },
-        handleChangePage(page, totalPage, spin) {
-            //通过正则把字符串中不是数字的内容都替换为空
-            let originalAngle = spin.style.transform.replace(/[^0-9]/ig, '');
-            if (originalAngle) {
-                originalAngle = parseInt(originalAngle, 10);
-            } else {
-                originalAngle = 0;
-            }
-            spin.style.transform = 'rotate(' + (originalAngle + 360) + 'deg';
-            if (page < totalPage) {
-                dispatch(actionCreators.changePage(page + 1));
-            } else {
-                dispatch(actionCreators.changePage(1));
-            }
-        }
+  return {
+    handleInputFocus(list) {
+      (list.size === 0) && dispatch(actionCreators.getList());
+      dispatch(actionCreators.searchFocus());
+    },
+    handleInputBlur() {
+      dispatch(actionCreators.searchBlur());
+    },
+    handleMouseEnter() {
+      dispatch(actionCreators.mouseEnter());
+    },
+    handleMouseLeave() {
+      dispatch(actionCreators.mouseLeave());
+    },
+    handleChangePage(page, totalPage, spin) {
+      //通过正则把字符串中不是数字的内容都替换为空
+      let originalAngle = spin.style.transform.replace(/[^0-9]/ig, '');
+      if (originalAngle) {
+        originalAngle = parseInt(originalAngle, 10);
+      } else {
+        originalAngle = 0;
+      }
+      spin.style.transform = 'rotate(' + (originalAngle + 360) + 'deg';
+      if (page < totalPage) {
+        dispatch(actionCreators.changePage(page + 1));
+      } else {
+        dispatch(actionCreators.changePage(1));
+      }
+    },
+    logout(){
+      dispatch(loginActionCreators.logout());
     }
+  }
 };
 
 
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,27 +2,46 @@ import React, {PureComponent} from 'react';
 import {connect} from 'react-redux';
 import {Input, LoginBox, LoginWrapper} from "./styles";
 import {Button} from "./styles";
+import * as actionCreators from "./store/actionCreators";
+import {Redirect} from "react-router-dom";
+
 
 class Login extends PureComponent {
   render() {
-    return (
-    <LoginWrapper>
-      <LoginBox>
-        <Input placeholder='账号'/>
-        <Input placeholder='密码'/>
-        <Button>登录</Button>
-      </LoginBox>
-    </LoginWrapper>
-    );
+    const {loginStatus} = this.props;
+    if (!loginStatus) {
+      return (
+        <LoginWrapper>
+          <LoginBox>
+            {/*使用ref来调用DOM*/}
+            <Input placeholder='账号' ref={(input) => {
+              this.account = input
+            }}/>
+            <Input placeholder='密码' type='password' ref={(input) => {
+              console.log(input);
+              this.password = input
+            }}/>
+            <Button onClick={() => this.props.login(this.account, this.password)}>登录</Button>
+          </LoginBox>
+        </LoginWrapper>
+      );
+    }else {
+      //使用重定向更新当前页面到首页
+      return <Redirect to='/'/>
+    }
+
   }
 }
 
 const mapStateToProps = (state) => ({
-
+  loginStatus: state.getIn(['login', 'login'])
 });
 
 const mapDispatchToProps = (dispatch) => ({
-
+  login(accountElem, passwordElem) {
+    console.log(accountElem);
+    dispatch(actionCreators.login(accountElem.value, passwordElem.value));
+  }
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
